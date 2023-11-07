c275a6e00a4052f1cd9da01af30eba8f890384ae
Douglas <douglas.dhcb89@gmail.com>
2023-11-06 22:15:52 -0300

    Revert "add game class"
    This reverts commit dfe2c3038c3b0e2e67866104e5ac081fae50e2f3.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
-#include "game.h"
+#include <SDL2/SDL.h>
 
 int main(int argc, char const *argv[])
 {
-    Game game;
+    SDL_Window* window = NULL;
 
-    game.run();
+    if ( SDL_Init(SDL_INIT_VIDEO) == 0 )
+        window = SDL_CreateWindow("game2d", 50, 50, 640, 480, 0);
+    else
+        SDL_Log("Erro na inicialização: %s", SDL_GetError());
 
+    if ( window != NULL )
+        while( !SDL_QuitRequested() ) {
+
+        }
+    else
+        SDL_Log("Erro na criação da janela: %s", SDL_GetError());
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
