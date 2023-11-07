dfe2c3038c3b0e2e67866104e5ac081fae50e2f3
Douglas <douglas.dhcb89@gmail.com>
2023-11-06 22:05:39 -0300

    add game class

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,10 @@
-#include <SDL2/SDL.h>
+#include "game.h"
 
 int main(int argc, char const *argv[])
 {
-    SDL_Window* window = NULL;
+    Game game;
 
-    if ( SDL_Init(SDL_INIT_VIDEO) == 0 )
-        window = SDL_CreateWindow("game2d", 50, 50, 640, 480, 0);
-    else
-        SDL_Log("Erro na inicialização: %s", SDL_GetError());
+    game.run();
 
-    if ( window != NULL )
-        while( !SDL_QuitRequested() ) {
-
-        }
-    else
-        SDL_Log("Erro na criação da janela: %s", SDL_GetError());
-
-    SDL_DestroyWindow(window);
-    SDL_Quit();
     return 0;
 }
